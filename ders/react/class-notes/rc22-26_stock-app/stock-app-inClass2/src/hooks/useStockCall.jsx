a9d510cef3abcd166460b61947d63de5a53d556b
yelizesgi <yelizesgi2014@gmail.com>
2023-08-16 19:53:34 +0300

    stock-app project session

--- ders/react/class-notes/rc22-26_stock-app/stock-app-inClass2/src/hooks/useStockCall.jsx
+++ ders/react/class-notes/rc22-26_stock-app/stock-app-inClass2/src/hooks/useStockCall.jsx
@@ -33,7 +33,33 @@ const useStockCall = () => {
     }
   }
 
-  return { getStockData, deleteStockData }
+  const postStockData = async (url, info) => {
+    dispatch(fetchStart())
+    try {
+      await axiosWithToken.post(`/stock/${url}/`, info)
+      toastSuccessNotify(`${url} succesfuly posted`)
+      getStockData(url)
+    } catch (error) {
+      dispatch(fetchFail())
+      toastErrorNotify(`${url} can not be posted`)
+      console.log(error)
+    }
+  }
+
+  const putStockData = async (url, info) => {
+    dispatch(fetchStart())
+    try {
+      await axiosWithToken.put(`/stock/${url}/${info.id}/`, info)
+      toastSuccessNotify(`${url} succesfuly updated`)
+      getStockData(url)
+    } catch (error) {
+      dispatch(fetchFail())
+      toastErrorNotify(`${url} can not be updated`)
+      console.log(error)
+    }
+  }
+
+  return { getStockData, deleteStockData, postStockData, putStockData }
 }
 
 export default useStockCall
--- ders/react/class-notes/rc22-26_stock-app/stock-app-inClass2/src/pages/Firms.jsx
+++ ders/react/class-notes/rc22-26_stock-app/stock-app-inClass2/src/pages/Firms.jsx
@@ -1,12 +1,13 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import Typography from "@mui/material/Typography"
-import Grid from "@mui/material/Grid"
 import Button from "@mui/material/Button"
-
-import { useDispatch, useSelector } from "react-redux"
+import Grid from "@mui/material/Grid"
+import { useSelector } from "react-redux"
 import useStockCall from "../hooks/useStockCall"
 import FirmCard from "../components/FirmCard"
+import FirmModal from "../components/FirmModal"
 // import axios from "axios"
+// import { useDispatch } from "react-redux"
 // import { fetchFail, fetchStart, getFirmsSuccess } from "../features/stockSlice"
 
 const Firms = () => {
@@ -33,29 +34,53 @@ const Firms = () => {
   const { getStockData } = useStockCall()
   const { firms } = useSelector((state) => state.stock)
 
+  const [info, setInfo] = useState({
+    name: "",
+    phone: "",
+    address: "",
+    image: "",
+  })
+
+  const [open, setOpen] = useState(false)
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => {
+    setOpen(false)
+    setInfo({ name: "", phone: "", address: "", image: "" })
+  }
+
   useEffect(() => {
     // getFirms()
     getStockData("firms")
   }, [])
 
-  console.log(firms)
   return (
     <div>
       <Typography variant="h4" color={"error"} mb={3}>
         Firms
       </Typography>
-      <Button variant="contained">NEW FIRM</Button>
+      <Button variant="contained" onClick={handleOpen}>
+        NEW FIRM
+      </Button>
+
+      <FirmModal
+        open={open}
+        handleClose={handleClose}
+        info={info}
+        setInfo={setInfo}
+      />
 
       <Grid container justifyContent={"center"} spacing={2}>
-        
-        {firms?.map((firm)=> (
-           <Grid item key={firm.id}>
-            <FirmCard firm={firm}/>
-           </Grid>
+        {firms?.map((firm) => (
+          <Grid item key={firm.id}>
+            <FirmCard
+              firm={firm}
+              handleOpen={handleOpen}
+              info={info}
+              setInfo={setInfo}
+            />
+          </Grid>
         ))}
-         
       </Grid>
-
     </div>
   )
 }
